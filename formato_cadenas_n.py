87ce721c15e5907e982e713a12eb3f9968d44491
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2026-02-28 21:01:48 -0400

    Create formato_cadenas_n.py

---
+++ formato_cadenas_n.py
@@ -0,0 +1,17 @@
+# Nuevo estilo de formato
+nombre = 'Juan'
+mi_cadena = 'hola, {}!'.format(nombre)
+
+print(mi_cadena)
+
+# convercion en hexadecimal
+error = 500
+cadena_hexa = 'Error en hexadecimal: {e1:x}'.format(e1=error)
+
+print(cadena_hexa)
+
+# convercion enteros a hexadecimal
+entero = 50
+cadena_f = 'Numero flotante: {entero:.2f}'.format(entero=entero)
+
+print(cadena_f)
