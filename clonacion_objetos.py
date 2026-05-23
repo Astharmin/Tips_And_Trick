d13d4dcf1cde42758a48850b1659c4911dd61d3a
Astharmin <salgadobarcenas96@gmail.com>
2026-05-23 01:47:40 -0400

    add deepcopy demo

--- clonacion_objetos.py
+++ clonacion_objetos.py
@@ -1,3 +1,5 @@
+import copy
+
 # Clonacion o copia de objetos
 
 # Ejemplo de Copina Superficial
@@ -23,3 +25,20 @@ print('\nLa copia solo fue superficial, solo se copia en referencia')
 
 print(f'\nLista a: {lista_a}')
 print(f'Lista b: {lista_b}')
+
+# Crear una copia profundas
+lista_c = [[1,2],[3,4],[5,6]]
+lista_d = copy.deepcopy(lista_c)
+
+# Comprobamos si son objetos distintos
+lista_c.append([7,8])
+print(f'\nSon distontos objetos (nivel superior)')
+print(f'Lista c: {lista_c}')
+print(f'Lista d: {lista_d}')
+
+# Comprobamos los objetos internos
+lista_c[0][1] = 'C'
+print(f'Lista c: {lista_c}')
+print(f'Lista d: {lista_d}')
+
+# Metodo copy sirve para realizar copias tipo shallow (Poco profundas)
