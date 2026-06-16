0588e77e633a59473fa1ddc2108f7c647355c6eb
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2026-06-16 13:45:53 -0400

    Demonstrate class vs instance variables

--- variables_clases_instancia.py
+++ variables_clases_instancia.py
@@ -5,6 +5,15 @@ class Perro:
     def __init__(self, nombre):
         self.nombre = nombre # <- Variable de instancia
 
+# Si modificamos la variable de clase se actualiza en todos los objetos
+Perro.num_patas = 5
+
+# Se puede crear una variable de clase desde la misma
+Perro.nombre = 'Clase perro'
+
+# Creamos una variable que no este asignada a los objetos
+Perro.num_orejas = 2
+
 if __name__ == '__main__':
     pongoso = Perro('Pongoso')
     chichigua = Perro('Chihigua')
@@ -19,3 +28,17 @@ if __name__ == '__main__':
 
     # No es posible acceder a la variable de instancia desde la clase
     # print(Perro.nombre)
+
+    # Se puede modificar la variable de clase individualmente
+    chichigua.num_patas = 6 # <- Aqui ocurre un detalle importante, se crea una variable de instancia temporal
+    print(f'\ncuantas patas tiene Chichigua?: {chichigua.num_patas}')
+
+    # Imprimimos el valor de la variable de instancia temporal y la variable de clase
+    print(f'Variable de instancia creada: {chichigua.num_patas}'
+          f'\nVariable de clase: {chichigua.__class__.num_patas}')
+
+    # Accedemos a las variables de clases creadas al vuelo
+    print(f'\n{Perro.nombre}\n{pongoso.nombre}')
+
+    # Accedemos a una variable de clase que no esta en las objetos
+    print(f'Cuantas orejas tiene el perro: {pongoso.num_orejas}')
