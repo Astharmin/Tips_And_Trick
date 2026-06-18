0d98be29ce0e2d4b77373f7109d8851410a88419
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2026-06-17 20:16:40 -0400

    Add __main__ demo of instance/class/static methods

--- metodos_instancia_clases_static.py
+++ metodos_instancia_clases_static.py
@@ -13,3 +13,26 @@ class MiClase:
     def metodo_estatico():
         return 'metodo estatico ejecutado...'
 
+if __name__ == '__main__':
+
+    # Caso 1: Ejecutamos el metodo de instancia implicita
+    objeto = MiClase()
+    print(f'Metodo iniciado: {objeto.metodo_instancia()}')
+
+    # Caso 2: Ejecutamos el metodo de instancia explicita
+    print(f'Metodo iniciado: {MiClase.metodo_instancia(objeto)}')
+
+    # Caso 3: Ejecutamos el metodo de instancia desde la clase
+    print(f'Metodo Iniciado: {MiClase.metodo_instancia(MiClase)}\n')
+
+    # Caso 4: Ejecutamos el metodo de clase implicita
+    print(f'Metodo Iniciado: {MiClase.metodo_clase()}')
+
+    # Caso 5: Metodo de clase desde las instancias
+    print(f'Metodo iniciado: {objeto.metodo_clase()}\n')
+
+    # Caso 6: Ejecutamos el metodo estatico
+    print(f'Metodo Iniciado: {MiClase.metodo_estatico()}')
+
+    # Caso 7: Ejecutamos el metodo estatico desde la instancia
+    print(f'Metodo iniciado: {objeto.metodo_estatico()}')
